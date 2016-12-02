a9d99d047851a2169254fdb44d1e2533a4bbdb37
Tanawat Tassana <amp.tanawat@gmail.com>
2016-12-02 19:40:45 +0700

    Display search result

--- lib/worker.ex
+++ lib/worker.ex
@@ -11,19 +11,20 @@ defmodule Movies.Worker do
 
     defp url_of(query), do: "http://www.omdbapi.com/?s=#{query}"
 
-    defp display_result({:ok, total_result}), do: "Total: #{total_result}"
+    defp display_result({:ok, search_results}), do: search_results
     defp display_result(:nothing), do: "Found nothing"
     defp display_result(_), do: "Unexpected result from the server"
 
     defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
         body
             |> JSON.decode!
-            |> get_total_results_from_json
+            |> get_search_results_from_json
             |> construct_result
     end
     defp parse_response(_), do: :error
 
     defp get_total_results_from_json(json), do: json["totalResults"]
+    defp get_search_results_from_json(json), do: json["Search"]
 
     defp construct_result(result) when is_nil(result), do: :nothing
     defp construct_result(result), do: {:ok, result}
