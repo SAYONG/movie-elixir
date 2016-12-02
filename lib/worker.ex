28a1dd93496576c81754e6bf68fe7b1700f7f8fb
Tanawat Tassana <amp.tanawat@gmail.com>
2016-12-02 19:57:46 +0700

    Make json data extraction more generic

--- lib/worker.ex
+++ lib/worker.ex
@@ -11,20 +11,19 @@ defmodule Movies.Worker do
 
     defp url_of(query), do: "http://www.omdbapi.com/?s=#{query}"
 
-    defp display_result({:ok, search_results}), do: search_results
+    defp display_result({:ok, results}), do: results
     defp display_result(:nothing), do: "Found nothing"
     defp display_result(_), do: "Unexpected result from the server"
 
     defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
         body
             |> JSON.decode!
-            |> get_search_results_from_json
+            |> get_json_data("Search")
             |> construct_result
     end
     defp parse_response(_), do: :error
 
-    defp get_total_results_from_json(json), do: json["totalResults"]
-    defp get_search_results_from_json(json), do: json["Search"]
+    defp get_json_data(json, path), do: json[path]
 
     defp construct_result(result) when is_nil(result), do: :nothing
     defp construct_result(result), do: {:ok, result}
